85cbfe81238bd21ef2a174a0f9b7209c0838e49e
yousef alfalag <yousifsalem774@gmail.com>
2024-09-18 17:19:02 +0300

    app Router

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,6 @@
+import 'package:dalel_app/features/splash/presentation/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router=GoRouter(routes: [
+  GoRoute(path: "/",builder: (context, state) => const SplashView(),)
+]);
